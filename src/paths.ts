ccc53910872e9295b3682a6cd098bd6add346254
Andrew Adhikari <hi@andrewadhikari.com>
2026-04-04 21:21:34 -0500

    feat: add database file path

--- src/paths.ts
+++ src/paths.ts
@@ -6,3 +6,4 @@ export const CONFIG_FILE = join(APP_DIR, "config.json");
 export const PORTFOLIO_FILE = join(APP_DIR, "portfolio.json");
 export const SESSION_DIR = join(APP_DIR, "sessions");
 export const WHOIS_HISTORY_DIR = join(APP_DIR, "whois-history");
+export const DB_FILE = join(APP_DIR, "domain-sniper.db");
